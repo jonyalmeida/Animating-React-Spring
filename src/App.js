f42703dc8986da1cc456fd23946e9a8efc92ea0c
jon <jon@jons-MacBook-Pro.local>
2020-12-26 10:56:06 -0600

    clean uneeded items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <button className='menu-button'>Menu</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
